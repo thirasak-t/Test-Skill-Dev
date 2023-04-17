00ec25a1b9f9b8ce56aeca761e0cb43d20c81d28
thirasak <nayzerotv@gmail.com>
2023-04-17 14:51:42 +0700

    connect database

---
+++ app-api/Context/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using app_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace app_api.Context
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+        
+    }
+}
---
+++ app-api/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace app_api.Models
+{
+    public class User
+    {
+        public User()
+        {
+            CreatedOn = DateTimeOffset.UtcNow;
+        }
+        [Required, Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PasswordHash { get; set; }
+        public byte[] ProfileImage { get; set; }
+        public DateTimeOffset CreatedOn { get; set; }
+        public DateTimeOffset LastModifiedOn { get; set; }
+
+
+    }
+}
